e913736755a827ab52d74a4e7f4efbdd6ea0fa57
Adnan Sattar <adnansattar00@hotmail.com>
2023-05-08 11:08:20 +0500

    added console app

---
+++ Mars/Math.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mars
+{
+    internal class Math
+    {
+        internal int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Mars/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var math = new Math();
+            var result = math.Sum(10 , 10);
+            Console.WriteLine("Sum is: " + result);
+            Console.ReadLine();
+        }
+    }
+}
